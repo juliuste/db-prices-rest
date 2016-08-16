a4f091f3475f514a3c5e918e8e1ec8f963e45b23
Jannis R <mail@jannisr.de>
2016-08-16 03:07:17 +0200

    handle timezones, reduce duration if current day

--- prices.js
+++ prices.js
@@ -7,11 +7,17 @@ const prices = require('db-prices')
 
 
 module.exports = (req, res, next) => {
-	let dt = new Date(req.query.date)
-	dt.setHours(0,0,0,0)
-	prices(req.query.from, req.query.to, dt)
+	const dt = moment(req.query.date).tz(config.timezone)
+	const now = moment().tz(config.timezone)
+
+	let duration = 24 * 60 // minutes
+	if (+moment(dt).startOf('day') === +moment(now).startOf('day'))
+		duration = Math.floor((moment(dt).endOf('day') - dt) / 1000 / 60)
+
+	prices(req.query.from, req.query.to, new Date(+dt), {duration})
 	.then((data) => {
 		for(let dat of data) delete dat.offer.routes;
-		res.json(data)}, next)
+		res.json(data)
+	}, next)
 	.catch(next)
 }
